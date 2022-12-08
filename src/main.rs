f5ea504ad6b3335879257f481bc8c01c7ae17d50
Alejandro Osornio <50227494+AOX0@users.noreply.github.com>
2022-12-08 00:45:25 -0600

    Init

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#![allow(non_snake_case)]
+
+use axum::routing::get;
+use axum::{extract::Path, response::Html, Router, Server};
+use leptos::*;
+
+#[component]
+fn HtmlC<'a>(
+    cx: Scope,
+    x_data: Option<&'a str>,
+    children: Box<dyn Fn() -> Vec<Element>>,
+) -> Element {
+    view! { cx,
+        <html>
+            <head>
+                <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer init></script>
+            </head>
+            <body x-data=x_data>
+            {children}
+            </body>
+        </html>
+    }
+}
+
+#[component]
+fn Hello(cx: Scope, name: String) -> Element {
+    view! {cx,
+        <HtmlC x-data="{value: '', count: 0, place: 'Type here...' }">
+            <div>
+                <input type="text" x-model="value" x-bind:placeholder="place" />
+            </div>
+            <div>
+                <p x-text="count"></p>
+                <button x-on:click="count++">"inc"</button>
+                <p>"Hola "{&name}</p>
+                <p x-text="value"></p>
+            </div>
+        </HtmlC>
+    }
+}
+
+async fn hello(Path(name): Path<String>) -> Html<String> {
+    Html(
+        "<!DOCTYPE html>".to_owned()
+            + &render_to_string(|cx| {
+                view! {cx,
+                    <Hello name=name/>
+                }
+            })
+            .replace("<!--/-->", "")
+            .replace("<!--#-->", ""),
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/hello/:name", get(hello));
+
+    Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap()
+}
